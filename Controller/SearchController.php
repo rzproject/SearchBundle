9e3bf0faabd4e137da4e6bf51b4ecb4c667cac6d
Mell Zamora <mellzamora@outlook.com>
2013-07-07 14:07:16 +0800

    updated controller

--- Controller/SearchController.php
+++ Controller/SearchController.php
@@ -23,37 +23,42 @@ class SearchController extends Controller
     {
         $search = $request->query->get('rz_q');
 
-        if (NULL !== $search) {
-            $config = $this->get('rz_search.config_manager');
-
-            //var_dump($config);
-            $client = $this->get('solarium.client');
-
-            // get a select query instance
-            $query = $client->createSelect();
-
-            // get highlighting component and apply settings
-            $hl = $query->getHighlighting();
-            $hl->setFields('content, description, title');
-            $hl->setSimplePrefix('<b>');
-            $hl->setSimplePostfix('</b>');
-
-            // set a query (all prices starting from 12)
-            //$query->setQuery($search);
-            $query->setQuery(sprintf('text:%s',$search));
-
-            // set start and rows param (comparable to SQL limit) using fluent interface
-            //$query->setStart(2)->setRows(20);
-
-            // this executes the query and returns the result
-            $resultset = $client->select($query);
-            $highlighting = $resultset->getHighlighting();
-
-            $response = $this->render('RzSearchBundle::results.html.twig', array('resultset'=>$resultset, 'highlighting'=>$highlighting));
+        if ( $search === '') {
+            $response = $this->render('RzSearchBundle::empty.html.twig');
             return $response;
-        } else {
+        }
+
+        if ($search === NULL) {
             $response = $this->render('RzSearchBundle::no_results.html.twig');
             return $response;
         }
+
+        $config = $this->get('rz_search.config_manager');
+
+        //var_dump($config);
+        $client = $this->get('solarium.client');
+
+        // get a select query instance
+        $query = $client->createSelect();
+
+        // get highlighting component and apply settings
+        $hl = $query->getHighlighting();
+        $hl->setFields('content, description, title');
+        $hl->setSimplePrefix('<b>');
+        $hl->setSimplePostfix('</b>');
+
+        // set a query (all prices starting from 12)
+        //$query->setQuery($search);
+        $query->setQuery(sprintf('text:%s',$search));
+
+        // set start and rows param (comparable to SQL limit) using fluent interface
+        //$query->setStart(2)->setRows(20);
+
+        // this executes the query and returns the result
+        $resultset = $client->select($query);
+        $highlighting = $resultset->getHighlighting();
+
+        $response = $this->render('RzSearchBundle::results.html.twig', array('resultset'=>$resultset, 'highlighting'=>$highlighting));
+        return $response;
     }
 }
