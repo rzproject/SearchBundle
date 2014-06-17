ae52b14a7e1cffb20d1fa5c3a2b8e929033826dc
mellzamora <mellzamora@outlook.com>
2014-06-17 12:21:49 +0800

    updated class reference

--- DependencyInjection/RzSearchExtension.php
+++ DependencyInjection/RzSearchExtension.php
@@ -11,10 +11,13 @@
 
 namespace Rz\SearchBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * This is the class that loads and manages your bundle configuration
